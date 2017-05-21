5718c86e4a84c45a754c54dc047ea9294efc628f
MarLeo <martin.aurele12@gmail.com>
2017-05-21 22:09:50 +0200

    european barrier option and posix thread driver V0.1
    Signed-off-by: MarLeo <martin.aurele12@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -4,69 +4,119 @@
 #include "lookback/header/LookbackOption.h"
 #include "barrier/header/BarrierOption.h"
 #include <chrono>
-#include <thread>
+#include <pthread.h>
 
-unsigned int __stdcall asianArithmeticCall(void *data);
+void *asianArithmeticCall(void *);
 
-unsigned int __stdcall asianArithmeticPut(void *data);
+void *asianArithmeticPut(void *);
 
-unsigned int __stdcall asianGeometricCall(void *data);
+void *asianGeometricCall(void *);
 
-unsigned int __stdcall asianGeometricPut(void *data);
+void *asianGeometricPut(void *);
 
-unsigned int __stdcall europeanCall(void *data);
+//void *europeanCall(void *);
 
-unsigned int __stdcall europeanPut(void *data);
+void *european(void *);
+
+//void *europeanLookbackCall(void *);
+
+void *europeanLookback(void *);
+
+void *europeanBarrier(void *);
+
+//void *europeanBarrierPut(void *);
+
+
+typedef struct {
+    int thread_id;
+} thread_data;
+
+//This function will be called from a thread
+
+void *call_from_thread(void *args) {
+    thread_data *my_data = (thread_data *) args;
+    std::cout << "Launched by thread " << my_data->thread_id << std::endl;
+    return NULL;
+}
 
 #define NUM_THREADS  6
 
+static const int num_threads = 10;
+
 int main(int argc, char *argv[]) {
 
-    /*
+    pthread_t threads[num_threads];
+
+
     std::chrono::time_point<std::chrono::system_clock> start, end;
     start = std::chrono::system_clock::now();
 
-    // create threads
-    HANDLE handle[NUM_THREADS];
+    pthread_create(&threads[0], NULL, european, NULL);
+    pthread_create(&threads[1], NULL, europeanLookback, NULL);
+    pthread_create(&threads[2], NULL, europeanBarrier, NULL);
 
-    handle[0] = (HANDLE) _beginthreadex(0, 0, &asianArithmeticCall, 0, 0, 0);
+    pthread_create(&threads[3], NULL, asianGeometricPut, NULL);
+    pthread_create(&threads[4], NULL, asianGeometricCall, NULL);
 
-    handle[1] = (HANDLE) _beginthreadex(0, 0, &europeanCall, 0, 0, 0);
-
-    handle[2] = (HANDLE) _beginthreadex(0, 0, &europeanPut, 0, 0, 0);
-
-    handle[3] = (HANDLE) _beginthreadex(0, 0, &asianArithmeticPut, 0, 0, 0);
-
-    //handle[4] = (HANDLE) _beginthreadex(0, 0,  &asianGeometricCall, 0, 0, 0);
-
-    //handle[5] = (HANDLE) _beginthreadex(0, 0,  &asianGeometricPut, 0, 0, 0);
+    pthread_create(&threads[5], NULL, asianArithmeticCall, NULL);
+    pthread_create(&threads[6], NULL, asianArithmeticPut, NULL);
 
 
-    // WaitForSingleObject(handle[0], INFINITE);
-    // WaitForSingleObject(handle[1], INFINITE);
-    // WaitForSingleObject(handle[2], INFINITE);
-    // WaitForSingleObject(handle[3], INFINITE);
-    //WaitForSingleObject(handle[4], INFINITE);
-    //WaitForSingleObject(handle[5], INFINITE);
 
-    // WaitForMultipleObjectsEx(6, handle, true, INFINITE, true);
-    WaitForMultipleObjects(4, handle, true, INFINITE);
+    pthread_join(threads[0], NULL);
+    pthread_join(threads[1], NULL);
 
+    pthread_join(threads[2], NULL);
+    pthread_join(threads[3], NULL);
 
-    CloseHandle(handle[0]);
-    CloseHandle(handle[1]);
-    CloseHandle(handle[2]);
-    CloseHandle(handle[3]);
-    CloseHandle(handle[4]);
-    //CloseHandle(handle[5]);
+    pthread_join(threads[4], NULL);
+    pthread_join(threads[5], NULL);
+
+    pthread_join(threads[6], NULL);
+
 
     end = std::chrono::system_clock::now();
 
     std::chrono::duration<double> elapsed_seconds = end - start;
 
     std::cout << "Elapsed total  Time: " << elapsed_seconds.count() << std::endl;
-     */
 
+
+    return 0;
+}
+
+
+/*void *europeanBarrierPut(void *) {
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    start = std::chrono::system_clock::now();
+    int num_sims = 10000;
+    double spot = 100.0;
+    double strike = 100.0;
+    double rate = 0.05;
+    double dividend = 0.05;
+    double volatility = 0.2;
+    double pas = 500.0;
+    double maturity = 1.0;
+    double barrier_put = 120.0;
+
+    EuropeanBarrierOption put(strike, spot, rate, 0.0, volatility, maturity, barrier_put);
+
+    double put_price = put(num_sims, pas, OptionType::PUT);
+
+    end = std::chrono::system_clock::now();
+
+    std::chrono::duration<double> elapsed_seconds = end - start;
+
+    std::cout << "European Barrier put price: " << put_price << std::endl;
+
+    std::cout << "Elapsed Time: " << elapsed_seconds.count() << std::endl;
+
+    std::cout << "\n" << std::endl;
+
+}
+ */
+
+void *europeanBarrier(void *) {
     std::chrono::time_point<std::chrono::system_clock> start, end;
     start = std::chrono::system_clock::now();
     int num_sims = 10000;
@@ -80,17 +130,10 @@ int main(int argc, char *argv[]) {
     double barrier_call = 20.0;
     double barrier_put = 120.0;
 
-    /*EuropeanLookback call(spot, rate, 0.0, volatility, maturity);
-    EuropeanLookback put(spot, rate, 0.0, volatility, maturity);
-
-
-    double call_price = call(num_sims, pas, OptionType::CALL);
-    double put_price = put(num_sims, pas, OptionType::PUT);
-     */
-
 
     EuropeanBarrierOption call(strike, spot, rate, 0.0, volatility, maturity, barrier_call);
     EuropeanBarrierOption put(strike, spot, rate, 0.0, volatility, maturity, barrier_put);
+
 
     double call_price = call(num_sims, pas, OptionType::CALL);
     double put_price = put(num_sims, pas, OptionType::PUT);
@@ -106,10 +149,74 @@ int main(int argc, char *argv[]) {
 
     std::cout << "Elapsed Time: " << elapsed_seconds.count() << std::endl;
 
-    return 0;
+    std::cout << "\n" << std::endl;
 }
 
-unsigned int europeanPut(void *data) {
+/*void *europeanLookbackCall(void *) {
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    start = std::chrono::system_clock::now();
+    int num_sims = 10000;
+    double spot = 100.0;
+    double strike = 100.0;
+    double rate = 0.05;
+    double dividend = 0.05;
+    double volatility = 0.2;
+    double pas = 500.0;
+    double maturity = 1.0;
+
+    EuropeanLookback call(spot, rate, 0.0, volatility, maturity);
+
+    double call_price = call(num_sims, pas, OptionType::CALL);
+
+    end = std::chrono::system_clock::now();
+
+    std::chrono::duration<double> elapsed_seconds = end - start;
+
+    std::cout << "European Barrier call price: " << call_price << std::endl;
+
+    std::cout << "Elapsed Time: " << elapsed_seconds.count() << std::endl;
+
+    std::cout << "\n" << std::endl;
+}
+ */
+
+
+void *europeanLookback(void *) {
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    start = std::chrono::system_clock::now();
+    int num_sims = 10000;
+    double spot = 100.0;
+    double strike = 100.0;
+    double rate = 0.05;
+    double dividend = 0.05;
+    double volatility = 0.2;
+    double pas = 500.0;
+    double maturity = 1.0;
+
+    EuropeanLookback call(spot, rate, 0.0, volatility, maturity);
+    EuropeanLookback put(spot, rate, 0.0, volatility, maturity);
+
+    double call_price = call(num_sims, pas, OptionType::CALL);
+
+    double put_price = put(num_sims, pas, OptionType::PUT);
+
+    end = std::chrono::system_clock::now();
+
+    std::chrono::duration<double> elapsed_seconds = end - start;
+
+    std::cout << "European Lookback call price: " << call_price << std::endl;
+
+    std::cout << "European Lookback put price: " << put_price << std::endl;
+
+    //std::cout << "Elapsed Time: " << elapsed_seconds.count() << std::endl;
+
+    std::cout << "\n" << std::endl;
+}
+
+
+
+/*
+void *europeanPut(void *) {
     std::chrono::time_point<std::chrono::system_clock> start, end;
     start = std::chrono::system_clock::now();
     int num_sims = 10000;
@@ -136,10 +243,11 @@ unsigned int europeanPut(void *data) {
 
     std::cout << "\n" << std::endl;
 
-    return 0;
+    //return 0;
 }
+*/
 
-unsigned int europeanCall(void *data) {
+void * european(void *) {
     std::chrono::time_point<std::chrono::system_clock> start, end;
     start = std::chrono::system_clock::now();
     int num_sims = 10000;
@@ -152,8 +260,10 @@ unsigned int europeanCall(void *data) {
     double maturity = 1.0;
 
     European call(strike, spot, rate, dividend, volatility, maturity);
+    European put(strike, spot, rate, dividend, volatility, maturity);
 
     double call_price = call(num_sims, pas, OptionType::CALL);
+    double put_price = put(num_sims, pas, OptionType::PUT);
 
 
     end = std::chrono::system_clock::now();
@@ -162,20 +272,18 @@ unsigned int europeanCall(void *data) {
 
     std::cout << "European call price: " << call_price << std::endl;
 
-    std::cout << "Elapsed Time: " << elapsed_seconds.count() << std::endl;
+    std::cout << "European put price: " << put_price << std::endl;
+
+    //std::cout << "Elapsed Time: " << elapsed_seconds.count() << std::endl;
 
     std::cout << "\n" << std::endl;
 
-    return 0;
 }
 
-unsigned int asianArithmeticCall(void *data) {
+void *asianArithmeticCall(void *) {
     std::chrono::time_point<std::chrono::system_clock> start, end;
     start = std::chrono::system_clock::now();
 
-    // First we create the parameter list
-    // Note that you could easily modify this code to input the parameters
-    // either from the command line or via a file
     unsigned num_sims = 100000;   // Number of simulated asset paths
     unsigned num_intervals = 250;  // Number of intervals for the asset path to be sampled
     double spot = 30.0;  // Option price
@@ -203,14 +311,12 @@ unsigned int asianArithmeticCall(void *data) {
     std::chrono::duration<double> elapsed_seconds = end - start;
 
     std::cout << "Arithmetic Asian call Price: " << discount_payoff_avg << std::endl;
-    std::cout << "Elapsed Time: " << elapsed_seconds.count() << std::endl;
+    //std::cout << "Elapsed Time: " << elapsed_seconds.count() << std::endl;
 
     std::cout << "\n" << std::endl;
-
-    return 0;
 }
 
-unsigned int __stdcall asianArithmeticPut(void *data) {
+void *asianArithmeticPut(void *) {
 
     std::chrono::time_point<std::chrono::system_clock> start, end;
     start = std::chrono::system_clock::now();
@@ -245,17 +351,13 @@ unsigned int __stdcall asianArithmeticPut(void *data) {
     std::chrono::duration<double> elapsed_seconds = end - start;
 
     std::cout << "Arithmetic Asian put Price: " << discount_payoff_avg << std::endl;
-    std::cout << "Elapsed Time: " << elapsed_seconds.count() << std::endl;
+    //std::cout << "Elapsed Time: " << elapsed_seconds.count() << std::endl;
 
     std::cout << "\n" << std::endl;
 
-    return 0;
-
-
-    return 0;
 }
 
-unsigned int __stdcall asianGeometricCall(void *data) {
+void *asianGeometricCall(void *) {
     std::chrono::time_point<std::chrono::system_clock> start, end;
     start = std::chrono::system_clock::now();
 
@@ -289,14 +391,12 @@ unsigned int __stdcall asianGeometricCall(void *data) {
     std::chrono::duration<double> elapsed_seconds = end - start;
 
     std::cout << "Geometric Asian call Price: " << discount_payoff_avg << std::endl;
-    std::cout << "Elapsed Time: " << elapsed_seconds.count() << std::endl;
+    //std::cout << "Elapsed Time: " << elapsed_seconds.count() << std::endl;
 
     std::cout << "\n" << std::endl;
-
-    return 0;
 }
 
-unsigned int __stdcall asianGeometricPut(void *data) {
+void * asianGeometricPut(void *) {
     std::chrono::time_point<std::chrono::system_clock> start, end;
     start = std::chrono::system_clock::now();
 
@@ -330,10 +430,8 @@ unsigned int __stdcall asianGeometricPut(void *data) {
     std::chrono::duration<double> elapsed_seconds = end - start;
 
     std::cout << "Geometric Asian put Price: " << discount_payoff_avg << std::endl;
-    std::cout << "Elapsed Time: " << elapsed_seconds.count() << std::endl;
+    //std::cout << "Elapsed Time: " << elapsed_seconds.count() << std::endl;
 
     std::cout << "\n" << std::endl;
-
-    return 0;
 }
 
